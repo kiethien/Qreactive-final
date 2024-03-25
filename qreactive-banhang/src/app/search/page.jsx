d07cbe33f63ea1c773d3483ccde543eb9e0f9dd1
quanghu <112391583+quanghu@users.noreply.github.com>
2024-03-25 23:25:58 +0700

    Update page.jsx

--- qreactive-banhang/src/app/search/page.jsx
+++ qreactive-banhang/src/app/search/page.jsx
@@ -43,7 +43,7 @@ END:VCARD`;
       a.href = url;
       a.download = `${qrCode.name}.vcf`;
       document.body.appendChild(a);
-      
+
       // Trigger the click event on the anchor element
       a.click();
 
@@ -55,50 +55,54 @@ END:VCARD`;
   };
 
   return (
-    <div>
-      <div className={MyQR.search}>
-        <input
-          type="text"
-          className={MyQR.search_input}
-          placeholder="Search for other people Vcard ...."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button
-          className={MyQR.search_button}
-          onClick={handleSearch}
-          disabled={!searchQuery.trim()}
-        >
-          Go
-        </button>
-      </div>
       <div>
-        <ul>
-          {searchResults.map((qrCode, index) => (
-            <li key={index}>
-              <div className={MyQR.qrCodeInfo}>
-                {/* Render each search result */}
-                <img src={qrCode.QRcode} alt={qrCode.name} style={{ width: '200px', height: '200px' }} />
-                <p>Name: {qrCode.name}</p>
-                <p>Email: {qrCode.email}</p>
-                <p>Company: {qrCode.company}</p>
-                <p>Position: {qrCode.position}</p>
-                <p>Phone: {qrCode.phone}</p>
-                <p>Address: {qrCode.address}</p>
-                <p>Website: {qrCode.website}</p>
-                <button
-                  className={MyQR.dropbtn}
-                  onClick={() => downloadVCard(qrCode)} // Pass the qrCode to the download function
-                >
-                  Download
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <div className={MyQR.searchContainer}>
+          <div className={MyQR.search}>
+            <input
+                type="text"
+                className={MyQR.search_input}
+                placeholder="Search for other people Vcard ...."
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button
+                className={MyQR.search_button}
+                onClick={handleSearch}
+                disabled={!searchQuery.trim()}
+            >
+              Go
+            </button>
+          </div>
+        </div>
+        <div className={MyQR.info}>
+          <ul>
+            {searchResults.map((qrCode, index) => (
+                <li key={index} className={MyQR.qrCodeItem}>
+                  <div className={MyQR.qrCodeInfo}>
+                    <img src={qrCode.QRcode} alt={qrCode.name} className={MyQR.qrCodeImage} />
+                    <div className={MyQR.content}>
+                      <p className={MyQR.qrCodeName}>Name: {qrCode.name}</p>
+                      <p>Email: {qrCode.email}</p>
+                      <p>Company: {qrCode.company}</p>
+                      <p>Position: {qrCode.position}</p>
+                      <p>Phone: {qrCode.phone}</p>
+                      <p>Address: {qrCode.address}</p>
+                      <p>Website: {qrCode.website}</p>
+                    </div>
+
+                    <button
+                        className={MyQR.downloadButton}
+                        onClick={() => downloadVCard(qrCode)}
+                    >
+                      Download
+                    </button>
+                  </div>
+                </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
   );
 }
 
-export default MyQRCodesContent;
+export default MyQRCodesContent
